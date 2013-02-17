6e1dc5fae889cea84ff44b0d6c967e87abdc9619
Harrison Wang <hwang0214@gmail.com>
2013-02-17 13:17:43 -0500

    ruby koans completed

---
+++ JukeBoxLab.rb
@@ -0,0 +1,23 @@
+ songs = [
+  "The Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "The Phoenix - Consolation Prizes"
+]
+# list, play, help, exit
+
+puts "Welcome to the Jukebox"
+puts "What is your command? (type help if you need further help)"
+input = gets.chomp
+
+
+songs.split(" - ").join(", ")
+#songs.split(" - ")
+def play(song)
+
+end
+
+puts play(input)
